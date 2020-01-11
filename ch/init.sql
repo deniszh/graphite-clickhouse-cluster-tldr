4033226a294eb55e03ec4e31f0333edcf9f23fd6
deniszh <denis.zhdanov@gmail.com>
2020-01-11 21:41:23 +0100

    still not working, but close

--- ch/init.sql
+++ ch/init.sql
@@ -1,29 +1,31 @@
-CREATE TABLE IF NOT EXISTS default.graphite_opt (
+CREATE DATABASE graphite;
+
+CREATE TABLE IF NOT EXISTS graphite.data (
     `Path` LowCardinality(String),
     `Value` Float64 CODEC(Gorilla, ZSTD(1)),
     `Time` UInt32 CODEC(DoubleDelta, LZ4),
     `Date` Date CODEC(DoubleDelta, LZ4),
     `Timestamp` UInt32 CODEC(DoubleDelta, LZ4)
-) ENGINE = GraphiteMergeTree('graphite_rollup')
+) ENGINE = ReplicatedGraphiteMergeTree('/clickhouse/tables/{shard}/graphite.data', '{replica}', 'graphite_rollup')
 PARTITION BY toYYYYMM(Date)
 ORDER BY (Path, Time);
 
-CREATE TABLE IF NOT EXISTS default.graphite_index_opt (
+CREATE TABLE IF NOT EXISTS graphite.index (
     `Date` Date CODEC(DoubleDelta, LZ4),
     `Level` UInt32 CODEC(DoubleDelta, LZ4),
     `Path` LowCardinality(String),
     `Version` UInt32 CODEC(DoubleDelta, LZ4)
-) ENGINE = ReplacingMergeTree(Version)
+) ENGINE = ReplicatedReplacingMergeTree('/clickhouse/tables/{shard}/graphite.index', '{replica}', Version)
 PARTITION BY toYYYYMM(Date)
 ORDER BY (Level, Path, Date);
 
-CREATE TABLE IF NOT EXISTS default.graphite_tagged_opt (
+CREATE TABLE IF NOT EXISTS graphite.tagged (
       Date Date CODEC(DoubleDelta, LZ4),
       Tag1 LowCardinality(String),
       Path LowCardinality(String),
       Tags Array(LowCardinality(String)),
       Version UInt32 CODEC(Delta, LZ4),
       Deleted UInt8 CODEC(Delta, LZ4)
-) ENGINE = ReplacingMergeTree(Version)
+) ENGINE = ReplicatedReplacingMergeTree('/clickhouse/tables/{shard}/graphite.tagged', '{replica}', Version)
 PARTITION BY toYYYYMM(Date)
 ORDER BY (Tag1, Path, Date);
