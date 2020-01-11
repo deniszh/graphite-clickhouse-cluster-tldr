dc1cbbc6da2ac66dcedd17710c5e06a8df5065c7
deniszh <denis.zhdanov@gmail.com>
2020-01-11 22:45:32 +0100

    Adding proper carbonapi

---
+++ cleanup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm -rf data/*
+mkdir -p data/carbon-clickhouse
+mkdir -p data/grafana
+mkdir -p data/graphite-clickhouse
+mkdir -p data/zk001/data
+mkdir -p data/zk001/datalog
+mkdir -p data/zk001/logs
+mkdir -p data/ch01/data
+mkdir -p data/ch02/data
+mkdir -p data/ch03/data
+mkdir -p data/ch04/data
+mkdir -p data/ch05/data
+mkdir -p data/ch06/data
+mkdir -p data/ch01/metadata
+mkdir -p data/ch02/metadata
+mkdir -p data/ch03/metadata
+mkdir -p data/ch04/metadata
+mkdir -p data/ch05/metadata
+mkdir -p data/ch06/metadata
